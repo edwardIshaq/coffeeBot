e86cca70d2de5ecd346e6137dd46a08c061de35c
Edward Ishaq <edward.ishaq@gmail.com>
2018-04-30 22:29:06 -0700

    working on the models and dialogs

--- models/models.go
+++ models/models.go
@@ -4,13 +4,13 @@ import (
 	"github.com/nlopes/slack"
 )
 
-//MenuOption for Dialogs or messageMenu
+// MenuOption for Dialogs or messageMenu
 type MenuOption struct {
 	Title string `json:"text"`
 	Value string `json:"value"`
 }
 
-//MenuOption creates a `Menu` with same title/value
+// NewMenuOption creates a `Menu` with same title/value
 func NewMenuOption(title string) MenuOption {
 	return MenuOption{
 		Title: title,
@@ -25,10 +25,10 @@ func (m MenuOption) attachmentOption() slack.AttachmentActionOption {
 	}
 }
 
-//Menu is an array of `Menu`
+// Menu is an array of `Menu`
 type Menu []MenuOption
 
-//ConvertToAttachmentActionOptions converts Menus ->
+// ConvertToAttachmentActionOptions converts Menus ->
 func (ms Menu) ConvertToAttachmentActionOptions() []slack.AttachmentActionOption {
 	var options []slack.AttachmentActionOption
 
