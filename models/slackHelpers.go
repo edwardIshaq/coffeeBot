f87d3da9f81cab8a5f6c255ffc9a7f8ce1c8efbe
Edward Ishaq <edward.ishaq@gmail.com>
2018-05-28 18:22:36 -0700

    cleanup dead code

--- models/slackHelpers.go
+++ models/slackHelpers.go
@@ -30,23 +30,6 @@ func MakeAttachmentOptionsFromMap(m map[string]string) []slack.AttachmentActionO
 	return actionOptions
 }
 
-// ------------------------------------------
-type menuResponse struct {
-	Type      string   `json:"type"`
-	Actions   []action `json:"actions"`
-	TriggerID string   `json:"trigger_id"`
-}
-
-type action struct {
-	Name            string        `json:"name"`
-	Type            string        `json:"type"`
-	SelectedOptions []optionValue `json:"selected_options"`
-}
-
-type optionValue struct {
-	Value string `json:"value"`
-}
-
 // PayloadResponse a generic action response
 type PayloadResponse struct {
 	ResponseType      string     `json:"type"`
